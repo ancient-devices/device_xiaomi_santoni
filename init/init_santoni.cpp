3d9d293940d9a51b09b486704902274f4dff1831
ItoRenz <ridwan.ri214@gmail.com>
2021-10-15 15:17:00 +0700

    Santoni: Init: Improve init_santoni.cpp
    Signed-off-by: krxyzn <rafaibrahim1211@gmail.com>

--- init/init_santoni.cpp
+++ init/init_santoni.cpp
@@ -31,9 +31,17 @@
 #include <stdlib.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+
+#include <android-base/file.h>
+#include <cstdlib>
+#include <fstream>
+#include <string.h>
+
 #include <sys/sysinfo.h>
+#include <unistd.h>
 
 #include <android-base/properties.h>
+
 #include "vendor_init.h"
 #include "property_service.h"
 #include "log/log.h"
@@ -47,6 +55,98 @@ char const *heapmaxfree;
 void check_device()
 {
     struct sysinfo sys;
+
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include "vendor_init.h"
+#include "property_service.h"
+
+#include <string>
+#include <vector>
+
+using android::base::GetProperty;
+
+std::vector<std::string> ro_props_default_source_order = {
+    "odm.",
+    "product.",
+    "system.",
+    "system_ext.",
+    "vendor.",
+    "",
+};
+
+void property_override(char const prop[], char const value[], bool add)
+{
+    auto pi = (prop_info *) __system_property_find(prop);
+
+    if (pi != nullptr) {
+        __system_property_update(pi, value, strlen(value));
+    } else if (add) {
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
+}
+
+void set_ro_build_prop(const std::string &prop, const std::string &value, bool product) {
+    std::string prop_name;
+
+    for (const auto &source : ro_props_default_source_order) {
+        if (product)
+            prop_name = "ro.product." + source + prop;
+        else
+            prop_name = "ro." + source + "build." + prop;
+
+        property_override(prop_name.c_str(), value.c_str(), true);
+    }
+}
+
+typedef struct variant_info {
+    std::string brand;
+    std::string device;
+    std::string marketname;
+    std::string model;
+    std::string build_description;
+    std::string build_fingerprint;
+} variant_info_t;
+
+void search_variant(const std::vector<variant_info_t> variants);
+void set_variant_props(const variant_info_t variant);
+
+void property_override(char const prop[], char const value[], bool add = true);
+void set_dalvik_heap_size();
+void set_ro_build_prop(const std::string &prop, const std::string &value, bool product = false);
+
+static const variant_info_t santoni_info = {
+    .brand = "Xiaomi",
+    .device = "santoni",
+    .marketname = "",
+    .model = "Redmi 4X",
+    .build_description = "santoni-user 7.1.2 N2G47H V9.5.10.0.NAMMIFD release-keys",
+    .build_fingerprint = "Xiaomi/santoni/santoni:7.1.2/N2G47H/V9.5.10.0.NAMMIFD:user/release-keys",
+};
+
+    std::string proc_cmdline;
+    android::base::ReadFileToString("/proc/cmdline", &proc_cmdline, true);
+    if (proc_cmdline.find("S88503") != proc_cmdline.npos)
+        set_variant_props(rolex_info);
+    else if (proc_cmdline.find("S88505") != proc_cmdline.npos)
+        set_variant_props(riva_info);
+}
+
+void vendor_load_properties() {
+    determine_device();
+    set_dalvik_heap_size();
+}
+
+void set_dalvik_heap_size()
+{
+    struct sysinfo sys;
+    char const *heapstartsize;
+    char const *heapgrowthlimit;
+    char const *heapsize;
+    char const *heapminfree;
+    char const *heapmaxfree;
+    char const *heaptargetutilization;
 
     sysinfo(&sys);
 
@@ -108,4 +208,15 @@ void vendor_load_properties()
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
 
+}
+
+void set_variant_props(const variant_info_t variant) {
+    set_ro_build_prop("brand", variant.brand, true);
+    set_ro_build_prop("device", variant.device, true);
+    set_ro_build_prop("marketname", variant.marketname, true);
+    set_ro_build_prop("model", variant.model, true);
+
+    set_ro_build_prop("fingerprint", variant.build_fingerprint);
+    property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint.c_str());
+    property_override("ro.build.description", variant.build_description.c_str());
 }
