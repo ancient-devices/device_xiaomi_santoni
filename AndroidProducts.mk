ea1b339f9e9c4f7359c7a9d359c2b2b4d8010e9f
Jabiyeff <cebiyevanar@gmail.com>
2021-11-13 17:50:58 +0400

    santoni: Add Syberia-Project OS support
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -24,3 +24,12 @@ COMMON_LUNCH_CHOICES += \
     octavi_santoni-userdebug \
     octavi_santoni-user \
     octavi_santoni-eng
+
+# Syberia-Project
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/syberia_santoni.mk
+
+COMMON_LUNCH_CHOICES += \
+    syberia_santoni-userdebug \
+    syberia_santoni-user \
+    syberia_santoni-eng
