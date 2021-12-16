5f6ad0c001de0eedafd514074a101273d33ec4ce
krxyzn <rafaibrahim1211@gmail.com>
2021-12-16 11:47:03 +0000

    Santoni: Updated AndroidProducts.mk and Moved BUILD_FINGERPRINT to init
    Signed-off-by: krxyzn <rafaibrahim1211@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,98 +1,23 @@
-# LineageOS (crDroid)
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_santoni.mk
+   $(LOCAL_DIR)/lineage_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_santoni-userdebug \
     lineage_santoni-user \
+    lineage_santoni-userdebug \
     lineage_santoni-eng
-
-# HavocOS
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/havoc_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    havoc_santoni-userdebug \
-    havoc_santoni-user \
-    havoc_santoni-eng
-
-# AOSiP
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/aosip_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    aosip_santoni-userdebug \
-    aosip_santoni-user \
-    aosip_santoni-eng
-
-# Bliss
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/bliss_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    bliss_santoni-userdebug \
-    bliss_santoni-user \
-    bliss_santoni-eng
-
-# Derpfest
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/derp_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    derp_santoni-userdebug \
-    derp_santoni-user \
-    derp_santoni-eng
-
-# Cherish
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/cherish_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    cherish_santoni-userdebug \
-    cherish_santoni-user \
-    cherish_santoni-eng
-
-# Pixel Experience
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/aosp_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    aosp_santoni-userdebug \
-    aosp_santoni-user \
-    aosp_santoni-eng
-
-# Corvus
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/corvus_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    corvus_santoni-userdebug \
-    corvus_santoni-user \
-    corvus_santoni-eng
-
-# RevengeOS
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/revengeos_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    revengeos_santoni-userdebug \
-    revengeos_santoni-user \
-    revengeos_santoni-eng
-
-# ArrowOS
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/arrow_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    arrow_santoni-userdebug \
-    arrow_santoni-user \
-    arrow_santoni-eng
-
-# dotOS
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/dot_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    dot_santoni-userdebug \
-    dot_santoni-user \
-    dot_santoni-eng
--- lineage_santoni.mk
+++ lineage_santoni.mk
@@ -52,11 +52,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 720
-	
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H V9.5.10.0.NAMMIFD release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/santoni/santoni:7.1.2/N2G47H/V9.5.10.0.NAMMIFD:user/release-keys"
