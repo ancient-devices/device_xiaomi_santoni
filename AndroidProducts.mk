06590fc42476c6799ee3b7f347a2ebb28c2efd5e
Krxyzn <72651636+Krxyzn@users.noreply.github.com>
2021-12-23 10:22:07 -0800

    Santoni: Cleanup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,35 +1,23 @@
-# ArrowOS
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_santoni.mk
+   $(LOCAL_DIR)/aosp_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_santoni-userdebug \
-    arrow_santoni-user \
-    arrow_santoni-eng
-
-# AEX
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/aosp_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    aosp_santoni-userdebug \
     aosp_santoni-user \
+    aosp_santoni-userdebug \
     aosp_santoni-eng
-
-# Octavi-OS
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/octavi_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    octavi_santoni-userdebug \
-    octavi_santoni-user \
-    octavi_santoni-eng
-
-# Syberia-Project
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/syberia_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    syberia_santoni-userdebug \
-    syberia_santoni-user \
-    syberia_santoni-eng
